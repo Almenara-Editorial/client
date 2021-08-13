97ae06332613613d2675bd6d1bd05d36eb618da2
Carlos Sluzala <wdcarloss@gmail.com>
2021-08-12 22:04:44 -0300

    Initial commit from Create Next App

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  webpackFinal: async (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src');
+    return config;
+  },
+  stories: [
+    "../src/components/**/stories.tsx"
+  ],
+  addons: [
+    "@storybook/addon-essentials",
+  ]
+}
