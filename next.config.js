5350208be7609a74c61797b687db7c399a0d34ab
wdcarlos <wdcarloss@gmail.com>
2021-09-28 20:09:04 -0300

    Update next.config,json

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  typescript: {
+    ignoreBuildErrors: true
+  },
   images: {
     domains: ['localhost']
   },
